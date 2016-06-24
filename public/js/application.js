25d67b572ff50708b85e741684c50e0ca51870a6
Chris Otto & Carrie Liang <pair+chrisotto.liangcarrie20@devbootcamp.com>
2016-06-24 13:55:08 -0700

    Add login/logout

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("a#logout").on("click", function(event) {
+    event.preventDefault();
+    $.post($(this).attr('href'), '_method=delete').done(function(responseHref) {
+      window.location.replace(responseHref);
+    })
+  })
 });
